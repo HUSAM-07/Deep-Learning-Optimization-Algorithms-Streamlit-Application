c3659abce0355a21668c114b35932a311e44e7d8
Mohammed Husamuddin <uni.husam@gmail.com>
2024-04-17 14:01:22 +0400

    push

--- pages/streamlit_app.py
+++ pages/streamlit_app.py
@@ -7,6 +7,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 from sklearn.metrics import accuracy_score
+from streamlit_drawable_canvas import st_canvas
+import cv2
+import torch
+import torchvision.transforms as T
+import torch.nn.functional as F
+from model import transform_image, predict_image
+import pandas as pd
+import plotly.graph_objects as go
+import gc
 
 # Front-end
 def main():
@@ -16,6 +25,8 @@ def main():
     n_epochs = st.sidebar.slider("Number of Epochs", 1, 50, 20)
     learning_rate = st.sidebar.slider("Learning Rate", 0.001, 0.1, 0.01)
     optimizer_type = st.sidebar.selectbox("Optimizer", ["SGD", "Adam"])
+    stroke_width = st.sidebar.slider("Brush width: ", 10, 30, 20)
+    drawing_mode = st.sidebar.checkbox("Drawing mode ?", True)
 
     if st.sidebar.button("Train"):
         train_model(n_epochs, learning_rate, optimizer_type)
@@ -130,6 +141,92 @@ def test_model(test_count):
     plt.tight_layout()
     st.pyplot(fig)
 
+#Enable garbage collection
+gc.enable()
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+class ResNet8(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        # 1 x 28 x 28
+        self.conv1 = conv_block(in_channels, 64) # 64 x 28 x 28
+        self.conv2 = conv_block(64, 128, pool=True) # 128 x 14 x 14
+        self.res1 = nn.Sequential(conv_block(128, 128), 
+                                  conv_block(128, 128)) # 128 x 14 x 14
+        
+        self.conv3 = conv_block(128, 256, pool=True)  # 256 x 7 x 7
+        self.res2 = nn.Sequential(conv_block(256, 256), 
+                                  conv_block(256, 256)) # 256 x 7 x 7
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(7),  # 256 x 1 x 1 since maxpool with 7x7
+                                        nn.Flatten(),    # 256*1*1 
+                                        nn.Dropout(0.2),
+                                        nn.Linear(256, num_classes))
+        
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
+
+
+
+def transform_image(image):
+    stats = ((0.1307), (0.3081))
+    my_transforms = T.Compose([
+
+                        T.ToTensor(),
+                        T.Normalize(*stats)
+
+                        ])
+
+    return my_transforms(image)
+
+
+
+
+@st.cache
+def initiate_model():
+
+    # Initiate model
+    in_channels = 1
+    num_classes = 10
+    model = ResNet8(in_channels, num_classes)
+    device = torch.device('cpu')
+    PATH = 'mnist-resnet.pth'
+    model.load_state_dict(torch.load(PATH, map_location=device))
+    model.eval()
+
+    return model
+
+
+
+
+def predict_image(img):
+    
+    # Convert to a batch of 1
+    xb = img.unsqueeze(0)
+
+    model = initiate_model()
+
+    # Get predictions from model
+    yb = model(xb)
+    # apply softamx
+    yb_soft = F.softmax(yb, dim=1)
+    # Pick index with highest probability
+    confidence , preds  = torch.max(yb_soft, dim=1)
+    gc.collect()
+    # Retrieve the class label, confidence and probabilities of all classes using sigmoid 
+    return preds[0].item(), math.trunc(confidence.item()*100), torch.sigmoid(yb).detach()
 
 if __name__ == "__main__":
     main()
